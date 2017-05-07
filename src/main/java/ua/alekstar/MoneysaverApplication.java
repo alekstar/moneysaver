f6412e4bd71f77b28022436a6ed60de2a4f6093a
Oleksandr Tarasenko <work.tarasenko@gmail.com>
2017-05-07 16:00:16 +0300

    initial commit with spring boot

---
+++ src/main/java/ua/alekstar/MoneysaverApplication.java
@@ -0,0 +1,12 @@
+package ua.alekstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneysaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneysaverApplication.class, args);
+    }
+}
