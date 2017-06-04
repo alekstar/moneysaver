5dfb1faf5fae4f238f21588fd9751c59c5841f5c
Oleksandr Tarasenko <work.tarasenko@gmail.com>
2017-06-04 15:07:02 +0300

    change annotations for AccountRepository and CurrencyRepository to @Repository

--- src/main/java/ua/alekstar/moneysaver/dao/AccountRepository.java
+++ src/main/java/ua/alekstar/moneysaver/dao/AccountRepository.java
@@ -1,10 +1,10 @@
 package ua.alekstar.moneysaver.dao;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import ua.alekstar.moneysaver.dao.entities.Account;
 
-@Component
+@Repository
 public interface AccountRepository extends CrudRepository<Account, Long> {
 
 }
--- src/main/java/ua/alekstar/moneysaver/dao/CurrencyRepository.java
+++ src/main/java/ua/alekstar/moneysaver/dao/CurrencyRepository.java
@@ -1,10 +1,10 @@
 package ua.alekstar.moneysaver.dao;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import ua.alekstar.moneysaver.dao.entities.Currency;
 
-@Component
+@Repository
 public interface CurrencyRepository extends CrudRepository<Currency, Long> {
 
     Currency readByIsoCode(String isoCode);
